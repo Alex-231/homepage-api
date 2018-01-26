ab5f242928afa21a4e20d347a0b248494a10f1fa
Alex <A13x231mcsl@gmail.com>
2018-01-26 03:50:24 +0000

    corrected dburl

--- server.js
+++ server.js
@@ -13,26 +13,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-    if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-        var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-            mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-            mongoUser = process.env[mongoServiceName + '_USER'];
-      
-        if (mongoHost && mongoPort && mongoDatabase) {
-          mongoURLLabel = mongoURL = 'mongodb://';
-          if (mongoUser && mongoPassword) {
-            mongoURL += mongoUser + ':' + mongoPassword + '@';
-          }
-          // Provide UI label that excludes user id and pw
-          mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-          mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-      
-        }
-      }
-
 //Get POSTS with body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -56,7 +36,7 @@ app.use(function(req, res, next) {
    });
 
 //Connect to db
-mongoose.connect(mongoURL, {useMongoClient: true});
+mongoose.connect(config.database, {useMongoClient: true});
 
 //front end routes
 app.use("", express.static('./app/front/index.html'));
