3d18b12b367a4894ac889e0ac507468f552394d0
Alex <A13x231mcsl@gmail.com>
2018-01-26 03:44:29 +0000

    OpenShift Setup

--- server.js
+++ server.js
@@ -8,7 +8,30 @@ var passport = require('passport');
 var jwt = require('jsonwebtoken');
 
 var config = require('./config/main');
-var port = 4000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+    if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+        var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+            mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+            mongoUser = process.env[mongoServiceName + '_USER'];
+      
+        if (mongoHost && mongoPort && mongoDatabase) {
+          mongoURLLabel = mongoURL = 'mongodb://';
+          if (mongoUser && mongoPassword) {
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
+          }
+          // Provide UI label that excludes user id and pw
+          mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+          mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+      
+        }
+      }
 
 //Get POSTS with body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +56,7 @@ app.use(function(req, res, next) {
    });
 
 //Connect to db
-mongoose.connect(config.database, {useMongoClient: true});
+mongoose.connect(mongoURL, {useMongoClient: true});
 
 //front end routes
 app.use("", express.static('./app/front/index.html'));
@@ -48,5 +71,5 @@ require('./app/routes/main')(app);
 app.use('/static', express.static('./app/static'));
 
 
-app.listen(port, 'localhost');
+app.listen(port, ip);
 console.log('Server running on localhost:' + port);
