cd6c226d419b731e0546f260b3222cd610ecf361
Alex <A13x231mcsl@gmail.com>
2018-01-30 18:12:15 +0000

    API Test

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.use("/manifest.json", express.static('./app/front/manifest.json'));
 app.use("/asset-manifest.json", express.static('./app/front/asset-manifest.json'));
 app.use("/service-worker.js", express.static('./app/front/service-worker.js'))
 app.use("/static", express.static("./app/front/static"));
-app.use("*", express.static('./app/front/index.html'))
 
 //Require routes.
 require('./app/routes/main')(app);
@@ -52,6 +51,10 @@ app.use('/static', express.static('./app/static'));
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500).send('Something bad happened!');
+  });
+
+  app.get('*', function(req, res){
+    res.status(404).sendfile('./app/front/index.html');
   });
   
 
